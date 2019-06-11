82e1fbd217afb4301f769a43dcffa203b7b41ba9
AviramNahon <aviramnahon@gmail.com>
2019-06-11 12:47:04 +0300

    Update testMob.js

--- testMob.js
+++ testMob.js
@@ -1,1 +1,11 @@
-mob.init();
+//mob
+var caps =
+{  
+  "appPackage": "com.android.calculator2",
+  "appActivity": "com.android.calculator2.Calculator",
+  "automationName": "UIAutomator2"
+}
+mob.transaction("Init");
+mob.init(caps);
+
+mob.waitForVisible("id=com.android.calculator2:id/digit_"+'${Num1}', 5000);
